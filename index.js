3141df465624db0f851e03adb37b9da7c5421508
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2018-12-15 17:27:41 -0600

    Make all buttons clickable
    Tests the buttons to be clickable:
    Using a for loop, iterate through each button.
    When a button is clicked, an alert message appears.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// jS file for drumkit 
+
+var drumkitLength = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < drumkitLength; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+
+    alert("Ouch!");
+  });
+}
