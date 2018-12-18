49652b424a24e431ae8acb93fee8896b25b8f562
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2018-12-18 12:54:30 -0600

    Add sound and background imgs
    Only one sound at this point
    Fixed background img css to actually show drums

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var drumkitLength = document.querySelectorAll(".drum").length;
 for (var i = 0; i < drumkitLength; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click",function (){
 
-    alert("Ouch!");
+    var audio = new Audio('sounds/tom-3.mp3');
+    audio.play();
   });
 }
