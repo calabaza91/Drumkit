de23d78cb5c9d59b989b8898df4c498d8f68c7f9
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2018-12-19 10:55:44 -0600

    Add audio files to each button
    Use switch case to add audio to each button when clicked

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-
 var drumkitLength = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < drumkitLength; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML;
+    var tom1 = new Audio('sounds/tom-1.mp3');
+    var tom2 = new Audio('sounds/tom-2.mp3');
+    var tom3 = new Audio('sounds/tom-3.mp3');
+    var tom4 = new Audio('sounds/tom-4.mp3');
+    var snare = new Audio('sounds/snare.mp3');
+    var crash = new Audio('sounds/crash.mp3');
+    var kick = new Audio('sounds/kick-bass.mp3');
 
-    var audio = new Audio('sounds/tom-3.mp3');
-    audio.play();
+
+
+    switch (buttonInnerHTML) {
+      case "w":
+        tom1.play();
+        break;
+      case "a":
+        tom2.play();
+        break;
+      case "s":
+        tom3.play();
+        break;
+      case "d":
+        tom4.play();
+        break;
+      case "j":
+        snare.play();
+        break;
+      case "k":
+        crash.play();
+        break;
+      case "l":
+        kick.play();
+        break;
+      default: console.log(buttonInnerHTML);
+    }
   });
 }
+
+
+// var audio = new Audio('sounds/tom-3.mp3');
+//audio.play();
