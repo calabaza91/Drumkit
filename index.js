60cf77e217ec00ea88c4623cf4476edacb6a5f5c
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2018-12-19 11:31:25 -0600

    Add Keyboard event listener
    Switch case moved to own function to be used for both clicks and keypresses

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
+
+//Detect button press 
 var drumkitLength = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < drumkitLength; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var buttonInnerHTML = this.innerHTML;
-    var tom1 = new Audio('sounds/tom-1.mp3');
-    var tom2 = new Audio('sounds/tom-2.mp3');
-    var tom3 = new Audio('sounds/tom-3.mp3');
-    var tom4 = new Audio('sounds/tom-4.mp3');
-    var snare = new Audio('sounds/snare.mp3');
-    var crash = new Audio('sounds/crash.mp3');
-    var kick = new Audio('sounds/kick-bass.mp3');
 
+    makeSound(buttonInnerHTML);
 
-
-    switch (buttonInnerHTML) {
-      case "w":
-        tom1.play();
-        break;
-      case "a":
-        tom2.play();
-        break;
-      case "s":
-        tom3.play();
-        break;
-      case "d":
-        tom4.play();
-        break;
-      case "j":
-        snare.play();
-        break;
-      case "k":
-        crash.play();
-        break;
-      case "l":
-        kick.play();
-        break;
-      default: console.log(buttonInnerHTML);
-    }
   });
 }
 
 
-// var audio = new Audio('sounds/tom-3.mp3');
-//audio.play();
+
+//Detect keypress
+document.addEventListener("keypress", (e) => {
+  makeSound(e.key);
+})
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+    default:
+      console.log(buttonInnerHTML);
+  }
+}
